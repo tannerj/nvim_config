5f3ed128e7028f6f70a3d3a60142275a4c504e58
Joel Tanner Jones <jonesjoe@ecu.edu>
2025-10-02 12:01:48 -0400

    Add config from Ubuntu WSL setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = 'master',
+  lazy = false, 
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "ruby"},
+      auto_install = true,
       highlight = { enable = true },
-      indent = { enable = true }
+      indent = { enable = true },
     })
   end
 }
